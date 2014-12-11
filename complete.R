40419bf271b2a392972e68e3bacc42e4711fa904
liuyun1217 <liuyun_ucas@163.com>
2014-12-11 20:34:50 +0800

    pollutantmean.R

---
+++ complete.R
@@ -0,0 +1,22 @@
+pollutantmean <- function(directory,id = 1:332) {
+    ## 'directory' 是长度为1的字符向量，指明
+    ## CSV文件的位置
+    files <- list.files(directory, full.names=TRUE)
+    ## 'pollutant' 是长度为1的字符向量，指明
+    ## 污染物的名称，我们将会计算其 , pollutant
+    ## 平均值; 要么是“硫酸盐(sulfate)”要么是“硝酸盐(nitrate)”
+    comdat <- data.frame()
+    ## 'id'是正整数向量，指明监测点的ID，
+    ## 将要被要使用的
+    for (i in id) {
+        seledat <- read.csv(files[i])
+        a <-(!is.na(seledat[,"nitrate"]))+(!is.na(seledat[,"sulfate"]))
+        1com <- sum(a==2)
+        comdat <- rbind(dat, c(1com,i)
+    }
+    
+    ## 返回列表内的所有监测点的污染物平均值，
+    ## “id”向量中的 (忽略 NA值)
+    comdat
+    
+}
---
+++ somefunctions.R
@@ -0,0 +1,14 @@
+pollutantmean <- function(directory) {
+  ## 'directory' 是长度为1的字符向量，指明
+  ## CSV文件的位置
+  setwd(directory)
+  ## 'pollutant' 是长度为1的字符向量，指明
+  ## 污染物的名称，我们将会计算其
+  ## 平均值; 要么是“硫酸盐(sulfate)”要么是“硝酸盐(nitrate)”
+  
+  ## 'id'是正整数向量，指明监测点的ID，
+  ## 将要被要使用的
+  ##csv <-read.csv(id.csv)
+  ## 返回列表内的所有监测点的污染物平均值，
+  ## “id”向量中的 (忽略 NA值)
+}
