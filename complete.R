409684bd2aa51cd580716a5985b2866bd7327421
liuyun1217 <liuyun_ucas@163.com>
2014-12-11 21:17:52 +0800

    complete2

--- complete.R
+++ complete.R
@@ -1,22 +1,24 @@
-pollutantmean <- function(directory,id = 1:332) {
+complete <- function(directory,id = 1:332) {
     ## 'directory' 是长度为1的字符向量，指明
     ## CSV文件的位置
     files <- list.files(directory, full.names=TRUE)
-    ## 'pollutant' 是长度为1的字符向量，指明
-    ## 污染物的名称，我们将会计算其 , pollutant
-    ## 平均值; 要么是“硫酸盐(sulfate)”要么是“硝酸盐(nitrate)”
+   
+    ##多次尝试无法为空的数据框列命名，所有在最后命名
     comdat <- data.frame()
-    ## 'id'是正整数向量，指明监测点的ID，
-    ## 将要被要使用的
+   
+    ##m每一次循环读取一天的观测数，利用是否NA来进行01化
+    ##加起来等于2的，即两TRUE才是完整的观测数
     for (i in id) {
         seledat <- read.csv(files[i])
         a <-(!is.na(seledat[,"nitrate"]))+(!is.na(seledat[,"sulfate"]))
-        1com <- sum(a==2)
-        comdat <- rbind(dat, c(1com,i)
+        onecom <- sum(a==2)
+        comdat <- rbind(comdat, c(i,onecom))
     }
     
-    ## 返回列表内的所有监测点的污染物平均值，
-    ## “id”向量中的 (忽略 NA值)
+    ##为列命名
+    names(comdat) <- c("id","nobs")
+    
+    ##输出
     comdat
     
 }
--- pollutantmean.R
+++ pollutantmean.R
@@ -2,18 +2,15 @@ pollutantmean <- function(directory, pollutant,id = 1:332) {
     ## 'directory' 是长度为1的字符向量，指明
     ## CSV文件的位置
     files <- list.files(directory, full.names=TRUE)
-    ## 'pollutant' 是长度为1的字符向量，指明
-    ## 污染物的名称，我们将会计算其 , pollutant
-    ## 平均值; 要么是“硫酸盐(sulfate)”要么是“硝酸盐(nitrate)”
+    ##定义一个空的数据框来装总的观测数据
     dat <- data.frame()
-    ## 'id'是正整数向量，指明监测点的ID，
-    ## 将要被要使用的
+    ##每循环一次，rbind进dat里
     for (i in id) {
         dat <- rbind(dat, read.csv(files[i]))
     }
+    ##根据所选的观测项，去除NA后作平均
     csvmean <- mean(dat[,pollutant],na.rm = TRUE)
     
-    ## 返回列表内的所有监测点的污染物平均值，
-    ## “id”向量中的 (忽略 NA值)
+    ## 输出
     csvmean
 }
