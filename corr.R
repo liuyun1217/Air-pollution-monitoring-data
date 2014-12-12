7e0b5a89e102f595c35cf6fa8c35270fdc76eec3
liuyun1217 <liuyun_ucas@163.com>
2014-12-12 22:52:33 +0800

    corr3!

--- corr.R
+++ corr.R
@@ -3,24 +3,26 @@ corr <- function(directory, threshold = 0,id = 1:332) {
     ## CSV文件的位置
     files <- list.files(directory, full.names=TRUE)
     ##定义一个空的数据框来装总的观测数据
-    comdat <- data.frame()
     dat <- data.frame()
-    ##m每一次循环读取一天的观测数，利用是否NA来进行01化
-    ##加起来等于2的，即两TRUE才是完整的观测数
-    for (i in id) {
-        seledat <- read.csv(files[i])
-        a <-(!is.na(seledat[,"nitrate"]))+(!is.na(seledat[,"sulfate"]))
-        onecom <- sum(a==2)
-        comdat <- rbind(comdat, c(i,onecom))
-        if(onecom>threshold)
+    cordat<- vector()
+    ##调用complete来输出具有完整观测的观测点
+    comdat<<-complete(directory)
+    for(i in id){
+        
+        if(comdat[i,2]>threshold)
         {
-            dat <- rbind(dat, read.csv(files[i]))
+            cavdat <<- read.csv(files[i])
+            dat <-na.omit(cavdat)
+            cordat <-rbind(cordat,round(cor(dat$sulfate,dat$nitrate),digits = 5))
         }
+        else{}
     }
 
-
-        dat <-na.omit(dat)
-        cor(dat["sulfate"],dat["nitrate"],method = c("pearson", "kendall", "spearman"))
+        
+        datx <<-dat
+        cordat <-as.vector(cordat)
+        cordat
+        
     
     
 }
